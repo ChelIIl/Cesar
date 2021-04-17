f03233af83898ade5419a25e7dac14482d45b685
ChelIIl <Game.den20@yandex.ru>
2021-04-17 17:17:32 +0300

    Добавьте файлы проекта.

---
+++ Cesar/CesarForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Cesar
+{
+    partial class CesarForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cesar_txt = new System.Windows.Forms.TextBox();
+            this.shifr_btn = new System.Windows.Forms.Button();
+            this.rashifr_btn = new System.Windows.Forms.Button();
+            this.refresh_btn = new System.Windows.Forms.Button();
+            this.cesar_res_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.step_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cesar_txt
+            // 
+            this.cesar_txt.Location = new System.Drawing.Point(12, 35);
+            this.cesar_txt.Multiline = true;
+            this.cesar_txt.Name = "cesar_txt";
+            this.cesar_txt.Size = new System.Drawing.Size(237, 157);
+            this.cesar_txt.TabIndex = 0;
+            // 
+            // shifr_btn
+            // 
+            this.shifr_btn.Location = new System.Drawing.Point(295, 224);
+            this.shifr_btn.Name = "shifr_btn";
+            this.shifr_btn.Size = new System.Drawing.Size(94, 23);
+            this.shifr_btn.TabIndex = 1;
+            this.shifr_btn.Text = "Шифровать";
+            this.shifr_btn.UseVisualStyleBackColor = true;
+            this.shifr_btn.Click += new System.EventHandler(this.shifr_btn_Click);
+            // 
+            // rashifr_btn
+            // 
+            this.rashifr_btn.Location = new System.Drawing.Point(395, 224);
+            this.rashifr_btn.Name = "rashifr_btn";
+            this.rashifr_btn.Size = new System.Drawing.Size(91, 23);
+            this.rashifr_btn.TabIndex = 2;
+            this.rashifr_btn.Text = "Расшифровать";
+            this.rashifr_btn.UseVisualStyleBackColor = true;
+            this.rashifr_btn.Click += new System.EventHandler(this.rashifr_btn_Click);
+            // 
+            // refresh_btn
+            // 
+            this.refresh_btn.Location = new System.Drawing.Point(12, 198);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(75, 23);
+            this.refresh_btn.TabIndex = 3;
+            this.refresh_btn.Text = "Сбросить";
+            this.refresh_btn.UseVisualStyleBackColor = true;
+            this.refresh_btn.Click += new System.EventHandler(this.refresh_btn_Click);
+            // 
+            // cesar_res_txt
+            // 
+            this.cesar_res_txt.Location = new System.Drawing.Point(255, 35);
+            this.cesar_res_txt.Multiline = true;
+            this.cesar_res_txt.Name = "cesar_res_txt";
+            this.cesar_res_txt.Size = new System.Drawing.Size(237, 157);
+            this.cesar_res_txt.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ввод текста";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(252, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Вывод результата";
+            // 
+            // step_txt
+            // 
+            this.step_txt.Location = new System.Drawing.Point(295, 198);
+            this.step_txt.Name = "step_txt";
+            this.step_txt.Size = new System.Drawing.Size(191, 20);
+            this.step_txt.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Сдвиг (до 32):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 254);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.step_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cesar_res_txt);
+            this.Controls.Add(this.refresh_btn);
+            this.Controls.Add(this.rashifr_btn);
+            this.Controls.Add(this.shifr_btn);
+            this.Controls.Add(this.cesar_txt);
+            this.Name = "Form1";
+            this.Text = "Cesar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cesar_txt;
+        private System.Windows.Forms.Button shifr_btn;
+        private System.Windows.Forms.Button rashifr_btn;
+        private System.Windows.Forms.Button refresh_btn;
+        private System.Windows.Forms.TextBox cesar_res_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox step_txt;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cesar/CesarForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cesar
+{
+    public partial class CesarForm : Form
+    {
+        String s, t = "";
+        int a;
+
+        public CesarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void refresh_btn_Click(object sender, EventArgs e)
+        {
+            cesar_txt.Clear();
+            cesar_res_txt.Clear();
+        }
+
+        private void rashifr_btn_Click(object sender, EventArgs e)
+        {
+            if (step_txt.Text == "" || a > 32 || a < 0)
+                MessageBox.Show("Введите сдвиг(до 32)!");
+
+            else
+            {
+                t = "";
+                cesar_res_txt.Clear();
+                a = Convert.ToInt32(step_txt.Text);
+                s = cesar_txt.Text;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (1103 >= s[i] + 1 - 1 && s[i] + 1 - 1 >= 1072)
+                    {
+                        if (s[i] - a < 1072)
+                        {
+                            t += 1104 - (1072 - (s[i] - a));
+                        }
+                        else
+                        {
+                            t += s[i] - a;
+                        }
+                    }
+                    else if (1071 >= s[i] + 1 - 1 && s[i] + 1 - 1 >= 1040)
+                    {
+                        if (s[i] - a < 1040)
+                        {
+                            t += 1072 - (1040 - (s[i] - a));
+                        }
+                        else
+                        {
+                            t += s[i] - a;
+                        }
+                    }
+                    else
+                    {
+                        t += s[i];
+                    }
+                }
+
+                ShifrReplace();
+            }
+        }
+
+        private void shifr_btn_Click(object sender, EventArgs e)
+        {
+            if (step_txt.Text == "" || a > 32 || a < 0)
+                MessageBox.Show("Введите сдвиг(до 32)!");
+
+            else
+            {
+                t = "";
+                cesar_res_txt.Clear();
+                a = Convert.ToInt32(step_txt.Text);
+                s = cesar_txt.Text;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == 'ё')
+                    {
+                        if (1077 + a > 1103)
+                        {
+                            t += 1071 + (1077 + a - 1103);
+                        }
+                        else
+                        {
+                            t += 1077 + a;
+                        }
+                    }
+                    else if (s[i] == 'Ё')
+                    {
+                        if (1045 + a > 1071)
+                        {
+                            t += 1039 + (1045 + a - 1071);
+                        }
+                        else
+                        {
+                            t += 1045 + a;
+                        }
+                    }
+                    else if (1103 >= s[i] + 1 - 1 && s[i] + 1 - 1 >= 1072)
+                    {
+                        if (s[i] + a > 1103)
+                        {
+                            t += 1071 + (s[i] + a - 1103);
+                        }
+                        else
+                        {
+                            t += s[i] + a;
+                        }
+                    }
+                    else if (1071 >= s[i] + 1 - 1 && s[i] + 1 - 1 >= 1040)
+                    {
+                        if (s[i] + a > 1071)
+                        {
+                            t += 1039 + (s[i] + a - 1071);
+                        }
+                        else
+                        {
+                            t += s[i] + a;
+                        }
+                    }
+                    else
+                    {
+                        t += s[i];
+                    }
+                }
+
+                ShifrReplace();
+            }
+        }
+
+        private void ShifrReplace()
+        {
+            for (int i = 0; i < t.Length; i++)
+            {
+                t = t.Replace("1040", "А");
+                t = t.Replace("1041", "Б");
+                t = t.Replace("1042", "В");
+                t = t.Replace("1043", "Г");
+                t = t.Replace("1044", "Д");
+                t = t.Replace("1045", "E");
+                t = t.Replace("1046", "Ж");
+                t = t.Replace("1047", "З");
+                t = t.Replace("1048", "И");
+                t = t.Replace("1049", "Й");
+                t = t.Replace("1050", "К");
+                t = t.Replace("1051", "Л");
+                t = t.Replace("1052", "М");
+                t = t.Replace("1053", "Н");
+                t = t.Replace("1054", "О");
+                t = t.Replace("1055", "П");
+                t = t.Replace("1056", "Р");
+                t = t.Replace("1057", "С");
+                t = t.Replace("1058", "Т");
+                t = t.Replace("1059", "У");
+                t = t.Replace("1060", "Ф");
+                t = t.Replace("1061", "Х");
+                t = t.Replace("1062", "Ц");
+                t = t.Replace("1063", "Ч");
+                t = t.Replace("1064", "Ш");
+                t = t.Replace("1065", "Щ");
+                t = t.Replace("1066", "Ъ");
+                t = t.Replace("1067", "Ы");
+                t = t.Replace("1068", "Ь");
+                t = t.Replace("1069", "Э");
+                t = t.Replace("1070", "Ю");
+                t = t.Replace("1071", "Я");
+                t = t.Replace("1072", "а");
+                t = t.Replace("1073", "б");
+                t = t.Replace("1074", "в");
+                t = t.Replace("1075", "г");
+                t = t.Replace("1076", "д");
+                t = t.Replace("1077", "е");
+                t = t.Replace("1078", "ж");
+                t = t.Replace("1079", "у");
+                t = t.Replace("1080", "и");
+                t = t.Replace("1081", "й");
+                t = t.Replace("1082", "к");
+                t = t.Replace("1083", "л");
+                t = t.Replace("1084", "м");
+                t = t.Replace("1085", "н");
+                t = t.Replace("1086", "о");
+                t = t.Replace("1087", "п");
+                t = t.Replace("1088", "р");
+                t = t.Replace("1089", "с");
+                t = t.Replace("1090", "т");
+                t = t.Replace("1091", "у");
+                t = t.Replace("1092", "ф");
+                t = t.Replace("1093", "х");
+                t = t.Replace("1094", "ц");
+                t = t.Replace("1095", "х");
+                t = t.Replace("1096", "ш");
+                t = t.Replace("1097", "щ");
+                t = t.Replace("1098", "ъ");
+                t = t.Replace("1099", "ы");
+                t = t.Replace("1100", "ь");
+                t = t.Replace("1101", "э");
+                t = t.Replace("1102", "ю");
+                t = t.Replace("1103", "я");
+            }
+
+            cesar_res_txt.Text = t;
+        }
+    }
+}
